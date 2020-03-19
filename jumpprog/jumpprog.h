f6b1618bd80b0c0db55bb25bdb81daa3e3590b56
ysngr <junord182109@gmail.com>
2020-03-19 12:11:04 +0900

    Update scanner

--- jumpprog/jumpprog.h
+++ jumpprog/jumpprog.h
@@ -15,38 +15,21 @@
 
 #define NAME_N 1     // Name
 #define IF_N 2       // if
-#define ELSE_N 3     // else//
-#define WHILE_N 4    // while//
-#define LOOP_N 5     // loop//
-#define RETURN_N 6   // return
-#define GOTO_N 7     // goto
-#define INT_N 8      // int
-#define NUM_N 9      // Number
-#define PLUS_N 10    // +//
-#define INC_N 11     // ++
-#define MINUS_N 12   // -//
-#define DEC_N 13     // --//
-#define CDEC_N 14    // --'
-#define MUL_N 15     // *//
-#define DIV_N 16     // ///
-#define MOD_N 17     // %//
-#define ASSIGN_N 18  // =
-#define EQUAL_N 19   // ==//
-#define NOTEQ_N 20   // !=//
-#define LE_N 21      // <//
-#define LEEQ_N 22    // <=//
-#define RE_N 23      // >
-#define REEQ_N 24    // >=//
-#define AND_N 25     // &&//
-#define OR_N 26      // ||//
-#define NOT_N 27     // !//
-#define LPAREN_N 28  // (
-#define RPAREN_N 29  // )
-#define LBRACE_N 30  // {
-#define RBRACE_N 31  // }
-#define COMMA_N 32   // ,
-#define COLON_N 33   // :
-#define SEMI_N 34    // ;
+#define RETURN_N 3   // return
+#define GOTO_N 4     // goto
+#define INT_N 5      // int
+#define NUM_N 6      // Number
+#define INC_N 7      // ++
+#define CDEC_N 8     // --'
+#define ASSIGN_N 9   // =
+#define RE_N 10      // >
+#define LPAREN_N 11  // (
+#define RPAREN_N 12  // )
+#define LBRACE_N 13  // {
+#define RBRACE_N 14  // }
+#define COMMA_N 15   // ,
+#define COLON_N 16   // :
+#define SEMI_N 17    // ;
 
 
 /* scan.c */
