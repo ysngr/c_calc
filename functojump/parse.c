fdd06d87e85022adcdefeffdc20bbe249f33a630
ysngr <junord182109@gmail.com>
2020-03-24 21:33:33 +0900

    Update atom-cond-stat converter

--- functojump/parse.c
+++ functojump/parse.c
@@ -656,12 +656,12 @@ static void atom_conditional_expression(void)
             generate_indent_str(var1);
             define_variable_explicitly(var1);
             generate(ASSIGN_N);
-            if( ope == LEEQ_N || ope == REEQ_N ){
-                generate_str("_add");
-                generate(LPAREN_N);
-                generate_str("1");
-                generate(COMMA_N);
-            }
+            // if( ope == LEEQ_N || ope == REEQ_N ){
+            //     generate_str("_add");
+            //     generate(LPAREN_N);
+            //     generate_str("1");
+            //     generate(COMMA_N);
+            // }
             generate_str("_sub");
             generate(LPAREN_N);
             if( ope == RE_N || ope == REEQ_N ){
@@ -676,10 +676,15 @@ static void atom_conditional_expression(void)
                 generate_str(expr1);
             }
             generate(RPAREN_N);
-            if( ope == LEEQ_N || ope == REEQ_N ){
-                generate(RPAREN_N);
-            }
+            // if( ope == LEEQ_N || ope == REEQ_N ){
+            //     generate(RPAREN_N);
+            // }
             generate(SEMI_N);
+            if( ope == LEEQ_N || ope == REEQ_N ){
+                generate_indent_str(var1);
+                generate(INC_N);
+                generate(SEMI_N);
+            }
             snprintf(cond_expr, MAXSTRLEN, "%s > 0", var1);
             break;
     }
