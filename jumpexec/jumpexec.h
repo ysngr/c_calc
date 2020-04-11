b47c67e8f68df988469a9aca557bc4e64a5c029e
ysngr <junord182109@gmail.com>
2020-04-11 23:35:37 +0900

    Create scan.c

--- jumpexec/jumpexec.h
+++ jumpexec/jumpexec.h
@@ -4,6 +4,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define True 1
+#define False 0
+
+#define MAXSTRLEN 128
+
+
+/* scan.c */
+extern char str[MAXSTRLEN];
+void initialize_scan(char*);
+int scan(void);
+void finalize_scan(void);
 
 
 #endif
